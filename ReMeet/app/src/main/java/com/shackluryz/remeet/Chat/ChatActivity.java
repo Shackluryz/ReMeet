1c0ce666c00581cab0f510b323e7f8cee88eeb3f
Shackluryz <alem.shackuryz@gmail.com>
2021-06-03 22:15:31 -0300

    Displaying Chat's Messages

--- ReMeet/app/src/main/java/com/shackluryz/remeet/Chat/ChatActivity.java
+++ ReMeet/app/src/main/java/com/shackluryz/remeet/Chat/ChatActivity.java
@@ -1,6 +1,5 @@
  package com.shackluryz.remeet.Chat;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,14 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.shackluryz.remeet.Matches.MatchesActivity;
-import com.shackluryz.remeet.Matches.MatchesAdapter;
-import com.shackluryz.remeet.Matches.MatchesObject;
 import com.shackluryz.remeet.R;
 
 import java.util.ArrayList;
@@ -30,9 +27,10 @@ import java.util.Map;
 
      private RecyclerView mRecyclerView;
      private RecyclerView.Adapter mChatAdapter;
-     private RecyclerView.LayoutManager mChatLayoutMananger;
+     private RecyclerView.LayoutManager mChatLayoutManager;
 
      private EditText mSendEditText;
+
      private Button mSendButton;
 
      private String currentUserID, matchId, chatId;
@@ -48,16 +46,16 @@ import java.util.Map;
 
         currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID).child("connections").child("matches").child(matchId).child("chatId");
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID).child("connections").child("matches").child(matchId).child("ChatId");
         mDatabaseChat = FirebaseDatabase.getInstance().getReference().child("Chat");
 
         getChatId();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
-        mRecyclerView.setHasFixedSize(true);
-        mChatLayoutMananger = new LinearLayoutManager(ChatActivity.this);
-        mRecyclerView.setLayoutManager(mChatLayoutMananger);
+        mRecyclerView.setHasFixedSize(false);
+        mChatLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        mRecyclerView.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new ChatAdapter(getDataSetChat(), ChatActivity.this);
         mRecyclerView.setAdapter(mChatAdapter);
 
@@ -88,21 +86,65 @@ import java.util.Map;
      }
 
      private void getChatId() {
-        mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                if(snapshot.exists()){
-                    chatId = snapshot.getValue().toString();
-                    mDatabaseChat = mDatabaseChat.child(chatId);
-                }
-            }
+         mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
+             @Override
+             public void onDataChange(DataSnapshot snapshot) {
+                 if (snapshot.exists()){
+                     chatId = snapshot.getValue().toString();
+                     mDatabaseChat = mDatabaseChat.child(chatId);
+                     getChatMessages();
+                 }
+             }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
+             @Override
+             public void onCancelled(DatabaseError databaseError) {
 
-            }
-        });
+             }
+         });
      }
+
+     private void getChatMessages() {
+         mDatabaseChat.addChildEventListener(new ChildEventListener() {
+             @Override
+             public void onChildAdded(DataSnapshot snapshot, String s) {
+                 if(snapshot.exists()){
+                     String message = null;
+                     String createdByUser = null;
+
+                     if(snapshot.child("text").getValue() != null){
+                         message = snapshot.child("text").getValue().toString();
+                     }
+                     if(snapshot.child("createdByUser").getValue() != null){
+                         createdByUser = snapshot.child("createdByUser").getValue().toString();
+                     }
+
+                     if(message!=null && createdByUser!=null){
+                         Boolean currentUserBoolean = false;
+                         if(createdByUser.equals(currentUserID)){
+                             currentUserBoolean = true;
+                         }
+                         ChatObject newMessage = new ChatObject(message, currentUserBoolean);
+                         resultsChat.add(newMessage);
+                         mChatAdapter.notifyDataSetChanged();
+                     }
+                 }
+
+             }
+             @Override
+             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+             }
+             @Override
+             public void onChildRemoved(DataSnapshot dataSnapshot) {
+             }
+             @Override
+             public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+             }
+             @Override
+             public void onCancelled(DatabaseError databaseError) {
+             }
+         });
+     }
+
 
      private ArrayList<ChatObject> resultsChat = new ArrayList<ChatObject>();
      private List<ChatObject> getDataSetChat() {
--- ReMeet/app/src/main/java/com/shackluryz/remeet/Chat/ChatViewHolders.java
+++ ReMeet/app/src/main/java/com/shackluryz/remeet/Chat/ChatViewHolders.java
@@ -1,20 +1,23 @@
 package com.shackluryz.remeet.Chat;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.shackluryz.remeet.R;
 
 public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
 
+    public TextView mMessage;
+    public LinearLayout mContainer;
+
     public ChatViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
+
+        mMessage = itemView.findViewById(R.id.message);
+        mContainer = itemView.findViewById(R.id.container);
     }
 
     @Override
